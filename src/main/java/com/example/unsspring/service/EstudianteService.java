a96e0937503f4ccfd23040a644220bce76d98c06
JOHAN <johamax@gmail.com>
2024-11-13 13:43:52 -0500

    v.1.4

--- src/main/java/com/example/unsspring/service/EstudianteService.java
+++ src/main/java/com/example/unsspring/service/EstudianteService.java
@@ -23,6 +23,28 @@ public class EstudianteService {
         return estudianteRepository.findById(id).orElse(null);
     }
 
-    
+    //Metodo para registrar a un nuevo estudiante
+    public Estudiante createEstudiante (Estudiante estudiante){
+        return estudianteRepository.save(estudiante);
+    }
+
+    //Metodo para editar un estudiante teniendo su id
+    public Estudiante updateEstudiante(Integer id, Estudiante estudiante){
+        //identificamos a la entidad estudiante sabiendo su id
+        Estudiante estudianteActual = estudianteRepository.findById(id).orElse(null);
+        if (estudianteActual!=null){
+            estudianteActual.setNombre(estudiante.getNombre());
+            estudianteActual.setApellido(estudiante.getApellido());
+            estudianteActual.setEmail(estudiante.getEmail());
+            return estudianteRepository.save(estudianteActual);
+        }
+        return null;
+    }
+
+    //Metodo para eliminar a un estudiante sabiendo su id
+    public void deleteEstudiante(Integer id){
+        estudianteRepository.deleteById(id);
+    }
+
 
 }
