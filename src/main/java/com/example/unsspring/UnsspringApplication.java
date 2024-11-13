438a3c7bf3c81fbb790c63b1c42d58f0d6d799d5
JOHAN <johamax@gmail.com>
2024-11-13 12:49:08 -0500

    first commit

---
+++ src/main/java/com/example/unsspring/UnsspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.unsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsspringApplication.class, args);
+	}
+
+}
