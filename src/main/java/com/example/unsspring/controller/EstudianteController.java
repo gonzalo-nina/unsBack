a922465e8c99c504af4f8c224c7eee96b89de00e
JOHAN <johamax@gmail.com>
2024-11-13 13:47:15 -0500

    v.1.5

---
+++ src/main/java/com/example/unsspring/controller/EstudianteController.java
@@ -0,0 +1,13 @@
+package com.example.unsspring.controller;
+
+import com.example.unsspring.model.Estudiante;
+import com.example.unsspring.service.EstudianteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.*;
+
+
+
+public class EstudianteController {
+    
+}
