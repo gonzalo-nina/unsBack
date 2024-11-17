c667eab6146c9be25e393ad25978311966e5a18b
JOHAN <johamax@gmail.com>
2024-11-17 16:51:07 -0500

    primer commit with postgresql

--- src/main/java/com/example/unsspring/controller/EstudianteController.java
+++ src/main/java/com/example/unsspring/controller/EstudianteController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
